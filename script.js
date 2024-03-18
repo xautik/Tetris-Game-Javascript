94d365e249076a2f8c73c9861e411e972916b967
Kayodee <avocarterp@gmail.com>
2024-03-18 12:34:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,85 +2,47 @@
   var isStart = false;
   var tetris = {
     // Existing code...
-
-    // New variables for timer and high score
-    startTime: null,
-    highScore: 0,
-
-    init: function () {
-      isStart = true;
-      this.startTime = new Date();
-      // Existing initialization code...
-    },
-
-    initTimer: function () {
-      var me = this;
-      var tLoop = function () {
-        var currentTime = new Date();
-        var elapsedTime = Math.floor((currentTime - me.startTime) / 1000); // in seconds
-        me.time = elapsedTime;
-        me.setInfo('time');
-        me.timer = setTimeout(tLoop, 1000); // Update time every second
-      };
-      this.timer = setTimeout(tLoop, 1000);
-    },
-
-    // New function to update high score
-    updateHighScore: function () {
-      if (this.score > this.highScore) {
-        this.highScore = this.score;
-        // You can add code here to display/update the high score in your UI
-      }
-    },
-
-    // Existing code...
-
-    gameOver: function () {
-      this.updateHighScore(); // Update high score when game over
-      // Existing game over code...
-    },
-
-    // Existing code...
   };
 
-  const btn = document.querySelector('#start');
-  btn.addEventListener('click', function () {
+  // Helper function to iterate over array elements
+  if (!Array.prototype.forEach) {
+    Array.prototype.forEach = function (callback, thisArg) {
+      var T, k;
+      if (this == null) {
+        throw new TypeError(' this is null or not defined');
+      }
+      var O = Object(this);
+      var len = O.length >>> 0;
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+      }
+      if (arguments.length > 1) {
+        T = thisArg;
+      }
+      k = 0;
+      while (k < len) {
+        var kValue;
+        if (k in O) {
+          kValue = O[k];
+          callback.call(T, kValue, k, O);
+        }
+        k++;
+      }
+    };
+  }
+
+  const startBtn = document.querySelector('#start');
+  startBtn.addEventListener('click', function () {
     if (!isStart) {
       tetris.init();
-      tetris.initTimer(); // Start the timer when game starts
+      isStart = true;
     }
   });
 
-  const btn2 = document.querySelector('#pause');
-  btn2.addEventListener('click', function () {
+  const pauseBtn = document.querySelector('#pause');
+  pauseBtn.addEventListener('click', function () {
     if (isStart) {
       tetris.togglePause();
     }
   });
-
 })();
-
-if (!Array.prototype.eachdo) {
-  Array.prototype.eachdo = function (fn) {
-    for (var i = 0; i < this.length; i++) {
-      fn.call(this[i], i);
-    }
-  };
-}
-if (!Array.prototype.remDup) {
-  Array.prototype.remDup = function () {
-    var temp = [];
-    for (var i = 0; i < this.length; i++) {
-      var bool = true;
-      for (var j = i + 1; j < this.length; j++) {
-        if (this[i] === this[j]) {
-          bool = false;
-        }
-      }
-      if (bool === true) {
-        temp.push(this[i]);
-      }
-    }
-    return temp;
-  };
-}
